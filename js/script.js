4a6be5b86747577c62d0709a8fcdbadf114e0887
Mariia Gushuley <maryshka.moroz@gmail.com>
2021-08-09 14:48:16 +0300

    lesson_01

---
+++ js/script.js
@@ -0,0 +1,105 @@
+// console.log(street);
+// street;
+
+// street = "fhjshf";
+// let user = "";
+// let User = "";
+// let _user = "";
+// const $user = "";
+// // const -user = ""
+// const userActive = true;
+// const obj1 = { a: 15 };
+// console.log(obj1);
+// obj1.a = 25;
+// console.log(obj1);
+
+// const BASE_FORCE = 45;
+// let name;
+// // console.log("String is amazing");
+// console.log(name);
+// name = "Bob";
+// console.log(name);
+// name = "Misha";
+// console.log(name);
+// let age = 18;
+// const person = "Barabashka";
+// person = "Misha";
+// const city;
+
+// let num = 7;
+// let num2 = 65.89;
+// console.log(typeof num);
+// console.log(typeof num2);
+
+// const val = "I am amazing string";
+// console.log(typeof val);
+// const val1 = "";
+// console.log(typeof val1);
+// console.log(val.length);
+// console.log(val1.length);
+// console.log(val.toUpperCase());
+
+const isActive = true;
+// console.log(typeof isActive);
+
+// let name;
+// console.log(typeof name);
+
+// //null
+// let price = null;
+// console.log(typeof price);
+
+// const userInput = prompt("Введіть ваш вік", "18");
+// console.log(userInput);
+// console.log(typeof userInput);
+// console.log(userInput.length);
+// const a1 = 1;
+// const a = "1";
+// console.log(a1);
+// console.log(a);
+// let word = " ";
+// console.log(word.length);
+// console.log(word[0]);
+
+// const userInput = confirm("Вам є 18?");
+// console.log(userInput);
+let num3 = 6;
+// num3 = num3 + 3;
+// num3 += 3;
+
+// num3++;
+console.log(5 >= 5);
+console.log(3 < 2);
+console.log(2 === "2");
+console.log(2 !== 4);
+
+console.log(!false);
+// num3--;
+// console.log(num3);
+const num1 = "5";
+const num2 = "2.76float";
+console.log(Number.isNaN(num1));
+console.log(Number.isNaN(num2));
+
+let s1 = "hello";
+let s2 = "Max";
+let s3 = "you learn js!";
+
+const res = `${s1} ${s2} ${s3}`;
+console.log(res);
+
+const age = 20;
+console.log(age > 20 || age < 30);
+
+// console.log(parseFloat(num2));
+// // console.log(num1.toFixed(4));
+// console.log(typeof num1);
+// const result = parseFloat(num1);
+// console.log(typeof result);
+// console.log(result);
+
+// console.log(10 + num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+// console.log(num1 % num2);
