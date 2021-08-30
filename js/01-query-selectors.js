345b548072c0b7b9545160f4cd27af267903c08d
Mariia Gushuley <maryshka.moroz@gmail.com>
2021-08-30 11:22:57 +0300

    added tasks

--- js/01-query-selectors.js
+++ js/01-query-selectors.js
@@ -1,25 +1,44 @@
-// const body = document.body;
-// console.log(document);
-// console.log(body);
+// // const body = document.body;
+// // console.log(document);
+// // console.log(body);
 
+// const magicBtn = document.querySelector('.js-magic-btn');
+// console.log(magicBtn);
+// /*
+//  * document.querySelector(selector) и document.querySelectorAll(selector)
+//  *   selector - любой валидный CSS-селектор
+//  *
+//  * Что возвращают?
+//  */
 
+// const navEl = document.querySelector('.site-nav');
+// console.log('navEl', navEl);
 
+// const navLinksEl = document.querySelectorAll('.site-nav__link');
+// console.log('navLinksEl', navLinksEl);
 
-const magicBtn = document.querySelector('.js-magic-btn');
-console.log(magicBtn);
-/*
- * document.querySelector(selector) и document.querySelectorAll(selector)
- *   selector - любой валидный CSS-селектор
- *
- * Что возвращают?
- */
+// /*
+//  * Document.querySelector* и Element.querySelector*
+//  */
+const form = document.querySelector(".form");
 
-const navEl = document.querySelector('.site-nav');
-console.log('navEl', navEl);
+form.addEventListener("submit", handleSubmit);
 
-const navLinksEl = document.querySelectorAll('.site-nav__link');
-console.log('navLinksEl', navLinksEl);
+function handleSubmit(event) {
+  event.preventDefault();
+  // console.log('hi');
+  console.log(event);
+  const {
+    elements: { login, password },
+  } = event.currentTarget;
 
-/*
- * Document.querySelector* и Element.querySelector*
- */
+  if (login.value === "" && password.value === "") {
+    return console.log("all fields are empty");
+  }
+  if (login.value === "" || password.value === "") {
+    return console.log("Please fill in all the fields!");
+  }
+
+  console.log(`Login: ${login.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
