53ce792f7dd3fa91919c4848e99fdb430c44710a
Mariia Gushuley <maryshka.moroz@gmail.com>
2021-09-06 13:12:37 +0300

    added files

---
+++ src/js/01-promise-api.js
@@ -0,0 +1,51 @@
+/*
+ * Создание промиса
+ *  - Класс Promise
+ *  - resolve
+ *  - reject
+ *  - Promise.prototype.then(onResolve, onReject)
+ */
+
+const promise = new Promise((resolve, reject) => {
+  const canFulfill = Math.random() > 0.5;
+
+  setTimeout(() => {
+    if (canFulfill) {
+      resolve('Промис выполнился успешно, с результатом (исполнен, fulfilled)');
+    }
+
+    reject('Промис выполнился с ошибкой (отклонён, rejected)');
+  }, 1000);
+});
+
+// promise.then(onFulfilled, onRejected);
+
+function onFulfilled(result) {
+  console.log('onFulfilled -> onFulfilled');
+  console.log(`✅ ${result}`);
+  return 5;
+}
+
+function onRejected(error) {
+  console.log('onRejected -> onRejected');
+  console.log(`❌ ${error}`);
+}
+
+/*
+ * Цепочки промисов (chaining)
+ * Promise.prototype.catch(error)
+ * Promise.prototype.finally()
+ */
+
+promise
+  .then(onFulfilled)
+  .then(x => {
+    console.log(x);
+
+    return 10;
+  })
+  .then(y => {
+    console.log(y);
+  })
+  .catch(error => console.log(error))
+  .finally(() => console.log('Я буду выполнен в любом случае'));
